261632aa751f6794cc87fa6ae29c3fb2780e434b
shinya-kaneshiro <armageddn1@gmail.com>
2020-09-01 01:34:57 +0000

    Finish login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200828215808) do
+ActiveRecord::Schema.define(version: 20200831143800) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20200828215808) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "remember_digest"
   end
 
 end

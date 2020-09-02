88a70f6bad20b4162691e666c01410da2f4016c1
shinya-kaneshiro <armageddn1@gmail.com>
2020-09-02 14:51:26 +0000

    Finish debug window

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
   
   def new
+    @user = User.new
   end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "新規登録に成功しました。"
+      # redirect_to @user
+    else
+      render :new
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root 'static_pages#top'
 
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  resource :users
 end

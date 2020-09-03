6a7fbcd010c96ed562ece85e61ffef1c9f497388
shinya-kaneshiro <armageddn1@gmail.com>
2020-09-03 00:43:03 +0000

    Finish users_index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200831143800) do
+ActiveRecord::Schema.define(version: 20200902230722) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20200831143800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end

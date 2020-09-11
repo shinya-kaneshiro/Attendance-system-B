a202b4120abcdbd892dae7cc06654265bb70f6d3
shinya-kaneshiro <armageddn1@gmail.com>
2020-09-11 02:20:36 +0000

    Finish kintai_henshuu_gamen

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,7 @@
 class AttendancesController < ApplicationController
+  before_action :set_user, only: [:edit_one_month, :update_one_month]
+  before_action :set_one_month, only: :edit_one_month
+  
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
 
   def update
@@ -19,4 +22,27 @@ class AttendancesController < ApplicationController
     end
     redirect_to @user
   end
+  
+  def edit_one_month
+  end
+  
+  def update_one_month
+    ActiveRecord::Base.transaction do
+      attendances_params.each do |id, item|
+        attendance = Attendance.find(id)
+        attendance.update_attributes!(item)
+      end
+    end
+    flash[:success] = "1ヵ月分の勤怠情報を更新しました。"
+    redirect_to user_url(date: params[:date])
+  rescue ActiveRecord::RecordInvalid
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
+  end
+  
+  private
+    # 1ヶ月分の勤怠情報を扱う。
+    def attendances_params
+      params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
+    end
 end
